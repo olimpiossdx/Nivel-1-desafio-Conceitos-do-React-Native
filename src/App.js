7cfbfa1464c161209cb1c1593e2d8527c457f655
Olimpio Pimenta <olimpiodx@gmail.com>
2020-11-06 22:31:24 -0300

    format code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
-import { SafeAreaView, View, FlatList, Text, StatusBar, StyleSheet, TouchableOpacity, } from "react-native";
-import api from "./services/api";
+import { SafeAreaView, View, FlatList, Text, StatusBar, StyleSheet, TouchableOpacity, } from 'react-native';
+import api from './services/api';
 
 export default function App() {
   const [repositories, setRepositories] = React.useState([]);
@@ -24,84 +24,73 @@ export default function App() {
     });
   }
 
-  return (
-    <>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-      <SafeAreaView style={styles.container}>
-        <FlatList
-          keyExtractor={repository => repository.id}
-          data={repositories}
-          renderItem={({ item: repository }) => {
-            return (<View style={styles.repositoryContainer}>
-              <Text style={styles.repository}>{repository.title}</Text>
-              <View style={styles.techsContainer}>
-                {repository.techs.map(tech =>
-                  <Text style={styles.tech}key={tech}>
-                    {tech}
-                  </Text>)}
-              </View>
+  return (<>
+    <StatusBar barStyle='light-content' backgroundColor='#7159c1' />
+    <SafeAreaView style={styles.container}>
+      <FlatList keyExtractor={repository => repository.id} data={repositories}
+        renderItem={({ item: repository }) => (<View style={styles.repositoryContainer}>
+          <Text style={styles.repository}>{repository.title}</Text>
 
-              <View style={styles.likesContainer}>
-                <Text
-                  style={styles.likeText}
-                  testID={`repository-likes-${repository.id}`}
-                >
-                  {repository.likes} curtidas
+          <View style={styles.techsContainer}>
+            {repository.techs.map(tech =>
+              <Text style={styles.tech} key={tech}>
+                {tech}
+              </Text>)}
+          </View>
+
+          <View style={styles.likesContainer}>
+            <Text style={styles.likeText} testID={`repository-likes-${repository.id}`}>
+              {repository.likes} curtidas
                 </Text>
-              </View>
+          </View>
 
-              <TouchableOpacity
-                style={styles.button}
-                onPress={() => handleLikeRepository(repository.id)}
-                testID={`like-button-${repository.id}`}
-              >
-                <Text style={styles.buttonText}>Curtir</Text>
-              </TouchableOpacity>
-            </View>
-            );
-          }}
-        />
-      </SafeAreaView>
-    </>
-  );
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleLikeRepository(repository.id)}
+            testID={`like-button-${repository.id}`}>
+            <Text style={styles.buttonText}>Curtir</Text>
+          </TouchableOpacity>
+        </View>)} />
+    </SafeAreaView>
+  </>);
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#7159c1",
+    backgroundColor: '#7159c1',
   },
   repositoryContainer: {
     marginBottom: 15,
     marginHorizontal: 15,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     padding: 20,
   },
   repository: {
     fontSize: 32,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   techsContainer: {
-    flexDirection: "row",
+    flexDirection: 'row',
     marginTop: 10,
   },
   tech: {
     fontSize: 12,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginRight: 10,
-    backgroundColor: "#04d361",
+    backgroundColor: '#04d361',
     paddingHorizontal: 10,
     paddingVertical: 5,
-    color: "#fff",
+    color: '#fff',
   },
   likesContainer: {
     marginTop: 15,
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   likeText: {
     fontSize: 14,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginRight: 10,
   },
   button: {
@@ -109,10 +98,10 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     fontSize: 14,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginRight: 10,
-    color: "#fff",
-    backgroundColor: "#7159c1",
+    color: '#fff',
+    backgroundColor: '#7159c1',
     padding: 15,
   },
 });
